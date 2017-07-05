a7de638e622a67479b806019ade296d93813a97c
dixu <278844023@qq.com>
2017-07-05 09:59:05 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,42 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindItemggg: function(){
+    console.log(1)
+    wx.navigateTo({
+      url: '../mango/mango'
+    })
+  },
+
+  bindItemiii: function () {
+    console.log(2)
+    wx.clearStorage();
+  },
+  
+  bindViewTap:function(){
+    console.log(4444)
+  },
+
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+      console.log(userInfo)
+    })
+  },
+  
+
+})
+
+
